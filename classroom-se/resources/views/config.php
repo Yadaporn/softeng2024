483564bf4fa0f4026dbc7c1ae78eb0996c461100
Yadaporn <77092898+Yadaporn@users.noreply.github.com>
2024-04-15 21:35:36 +0700

    แก้ google ในconfig.php

--- classroom-se/resources/views/config.php
+++ classroom-se/resources/views/config.php
@@ -1,12 +1,1 @@
-<?php
-require_once "google-api/vendor/autoload.php";
-$gClient = new Google_Client();
-$gClient->setClientId("clientId");//ใส่ClientId
-$gClient->setClientSecret("ClientSecret");//ใส่ClientSecret
-$gClient->setApplicationName("Vicode Media Login");
-$gClient->setRedirectUri("http://localhost/Login/controller.php");
-$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
-
-// login URL
-$login_url = $gClient->createAuthUrl();
-?>
+//ใส่config ของgoogle
