839df7f5dca5bd2e6f96cd45972de71576e83326
Yadaporn <77092898+Yadaporn@users.noreply.github.com>
2024-04-15 21:34:06 +0700

    เปลี่ยนข้อมูลใน google

--- classroom-se/resources/views/config.php
+++ classroom-se/resources/views/config.php
@@ -1,8 +1,8 @@
 <?php
 require_once "google-api/vendor/autoload.php";
 $gClient = new Google_Client();
-$gClient->setClientId("639026804890-htq2om2hfqbp51t9s3le81lk7sbdn2fn.apps.googleusercontent.com");
-$gClient->setClientSecret("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");
+$gClient->setClientId("clientId");//ใส่ClientId
+$gClient->setClientSecret("ClientSecret");//ใส่ClientSecret
 $gClient->setApplicationName("Vicode Media Login");
 $gClient->setRedirectUri("http://localhost/Login/controller.php");
 $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
